7d0bfd47132b86450b3adf4026d6ab8ef07fd268
TasosTsiam <mfs170lvl@hotmail.com>
2023-05-03 15:47:59 +0300

    Added logo and table element

--- little-lemon/src/components/Footer.js
+++ little-lemon/src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ReactLogo from '../logo.svg';
 
 
 function Footer() {
     return (
-        <div></div>
+        <div>
+            <img src={ReactLogo} alt="React Logo"/>
+
+            <table>
+                <tr>
+                    <th>Doormat Navigation</th>
+                    <th>Contact</th>
+                    <th>Social Media links</th>
+                </tr>
+            </table>
+        </div>
     );
 }
 
