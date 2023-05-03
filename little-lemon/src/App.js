a8645f5d0b032ed0d54d4c95b0677de7615bdc0e
TasosTsiam <mfs170lvl@hotmail.com>
2023-05-03 14:24:35 +0300

    Created Nav component, imported it on App.js

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import './components/Nav';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Nav/>
       Homepage
-    </div>
+    </>
   );
 }
 
