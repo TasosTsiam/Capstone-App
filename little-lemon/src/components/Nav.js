160361ff78d73f924db171989229a0fd0503371c
TasosTsiam <mfs170lvl@hotmail.com>
2023-05-17 15:49:12 +0300

    Brought back older Nav.js code, to rework on it

--- little-lemon/src/components/Nav.js
+++ little-lemon/src/components/Nav.js
@@ -3,19 +3,15 @@ import React from 'react';
 
 function Nav() {
     return (
-        <nav className="navbar navbar-expand-md nav_container">
-            <a href="../../public/index.html" className="navbar-brand">Name</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bttttn">
-                <i className="bx bx-menu"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="bttttn">
-                <ul className="navbar-nav">
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">Home</a></li>
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">About</a></li>
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">Menu</a></li>
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">Reservations</a></li>
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">Order Online</a></li>
-                    <li className="nav-item"><a href="../../public/index.html" className="nav-link">Login</a></li>
+        <nav className="d-flex justify-content-center nav_container">
+            <div>
+                <ul>
+                    <li><a href="../../public/index.html">Home</a></li>
+                    <li><a href="../../public/index.html">About</a></li>
+                    <li><a href="../../public/index.html">Menu</a></li>
+                    <li><a href="../../public/index.html">Reservations</a></li>
+                    <li><a href="../../public/index.html">Order Online</a></li>
+                    <li><a href="../../public/index.html">Login</a></li>
                 </ul>
             </div>
         </nav>
